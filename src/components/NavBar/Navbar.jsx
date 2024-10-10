39a70611bb54d11625c2b317b1ba703683bfb038
DANNJUGUNA <ndan30071@gmail.com>
2024-10-10 13:10:02 +0300

    added the navbar contents

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+  return <header>
+    <div className="container">
+        {/* Log section */}
+        <div>
+            <h1>Log</h1>
+        </div>
+        {/* Navigation section */}
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li> 
+            <li>
+                <a href="#">About Us</a>
+            </li>
+            <li>
+                <a href="#">Products</a>
+            </li> 
+            <li>
+                <a href="#">Customer service</a>
+            </li>
+             <li>
+                <a href="#">Blogs</a>
+            </li>
+        </ul>
+        {/* button section */}
+        <div>
+            <button>Get in Touch</button>
+        </div>
+    </div>
+  </header>
 }
 
 export default Navbar
